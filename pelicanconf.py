ea5b58c4213a5ecb21b87c05b010e70e67362c22
Subhash T M <tmsubhash2020@gmail.com>
2025-09-18 21:42:23 +0530

    changes made

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,8 @@
 
 
-AUTHOR          = '<your name>'  # use your name
-SITENAME        = 'title' # use title
-GITHUB_USERNAME = 'tactlabs' # use your username
+AUTHOR          = 'Subhash T M'  # use your name
+SITENAME        = 'My python notes' # use title
+GITHUB_USERNAME = 'Subhashtm' # use your username
 
 
 # Blogroll
@@ -12,8 +12,8 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ("GitHub", "https://github.com/<yourusername>/"),
-    ("LinkedIn", "<linkedin handle>"),
+    ("GitHub", "https://github.com/Subhashtm/"),
+    ("LinkedIn", "subhashtm"),
 )
 
 ######## Advanced Settings (not recommended to to edited; take your own risk to touch the settings below) #############
